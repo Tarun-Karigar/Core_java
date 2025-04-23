0c9b5f8f8c56600b9524047302b9efcf47f57cbf
Tarun-Karigar <tarunkarigar9@gmail.com>
2025-04-23 21:07:49 +0530

    interface

---
+++ April_22/src/com/xworkz/tarun/runner/Interface_Runner.java
@@ -0,0 +1,341 @@
+package com.xworkz.tarun.runner;
+
+import com.xworkz.tarun.external.*;
+import com.xworkz.tarun.internal.rule.*;
+
+public class Interface_Runner {
+    public static void main(String[] args) {
+        Interface1 interface1=new Interface_Impl1();
+        interface1.m1();
+        Interface_Impl1 interfaceImpl1=new Interface_Impl1();
+        interfaceImpl1.m1();
+
+        Interface2 interface2=new Interface_Impl2();
+        interface2.m1();
+        Interface_Impl2 interfaceImpl2=new Interface_Impl2();
+        interfaceImpl2.m1();
+
+
+
+        Interface4 interface4=new Interface_Impl4();
+        interface4.m1();
+        Interface_Impl4 interface_impl4 =new Interface_Impl4();
+        interface_impl4.m1();
+
+
+        Interface5 interface5=new Interface_Impl5();
+        interface5.m1();
+        Interface_Impl5 interfaceImpl5=new Interface_Impl5();
+        interfaceImpl5.m1();
+
+        Interface6 interface6=new Interface_Impl6();
+        interface6.m1();
+        interface6.m2();
+        interface6.m3();
+        Interface_Impl6 interfaceImpl6=new Interface_Impl6();
+        interfaceImpl6.m1();
+        interfaceImpl6.m2();
+        interfaceImpl6.m3();
+
+        Interface7 interface7 = new Interface_Impl7();
+        interface7.m1();
+        interface7.m2();
+        interface7.m3();
+        Interface_Impl7 interfaceImpl7 = new Interface_Impl7();
+        interfaceImpl7.m1();
+        interfaceImpl7.m2();
+        interfaceImpl7.m3();
+
+        Interface8 interface8 = new Interface_Impl8();
+        interface8.m1();
+        interface8.m2();
+        interface8.m3();
+        Interface_Impl8 interfaceImpl8 = new Interface_Impl8();
+        interfaceImpl8.m1();
+        interfaceImpl8.m2();
+        interfaceImpl8.m3();
+
+        Interface9 interface9 = new Interface_Impl9();
+        interface9.m1();
+        interface9.m2();
+        interface9.m3();
+        Interface_Impl9 interfaceImpl9 = new Interface_Impl9();
+        interfaceImpl9.m1();
+        interfaceImpl9.m2();
+        interfaceImpl9.m3();
+
+        Interface10 interface10 = new Interface_Impl10();
+        interface10.m1();
+        interface10.m2();
+        interface10.m3();
+        Interface_Impl10 interfaceImpl10 = new Interface_Impl10();
+        interfaceImpl10.m1();
+        interfaceImpl10.m2();
+        interfaceImpl10.m3();
+
+        Interface11 interface11 = new Interface_Impl11();
+        interface11.m1();
+        interface11.m2();
+        interface11.m3();
+        interface11.m4();
+        interface11.m5();
+        Interface_Impl11 interfaceImpl11 = new Interface_Impl11();
+        interfaceImpl11.m1();
+        interfaceImpl11.m2();
+        interfaceImpl11.m3();
+        interfaceImpl11.m4();
+        interfaceImpl11.m5();
+
+        Interface12 interface12 = new Interface_Impl12();
+        interface12.m1();
+        interface12.m2();
+        interface12.m3();
+        interface12.m4();
+        interface12.m5();
+        Interface_Impl12 interfaceImpl12 = new Interface_Impl12();
+        interfaceImpl12.m1();
+        interfaceImpl12.m2();
+        interfaceImpl12.m3();
+        interfaceImpl12.m4();
+        interfaceImpl12.m5();
+
+        Interface13 interface13 = new Interface_Impl13();
+        interface13.m1();
+        interface13.m2();
+        interface13.m3();
+        interface13.m4();
+        interface13.m5();
+        Interface_Impl13 interfaceImpl13 = new Interface_Impl13();
+        interfaceImpl13.m1();
+        interfaceImpl13.m2();
+        interfaceImpl13.m3();
+        interfaceImpl13.m4();
+        interfaceImpl13.m5();
+
+        Interface14 interface14 = new Interface_Impl14();
+        interface14.m1();
+        interface14.m2();
+        interface14.m3();
+        interface14.m4();
+        interface14.m5();
+        Interface_Impl14 interfaceImpl14 = new Interface_Impl14();
+        interfaceImpl14.m1();
+        interfaceImpl14.m2();
+        interfaceImpl14.m3();
+        interfaceImpl14.m4();
+        interfaceImpl14.m5();
+
+
+        Interface15 interface15 = new Interface_Impl15();
+        interface15.m1();
+        interface15.m2();
+        interface15.m3();
+        interface15.m4();
+        interface15.m5();
+        Interface_Impl15 interfaceImpl15 = new Interface_Impl15();
+        interfaceImpl15.m1();
+        interfaceImpl15.m2();
+        interfaceImpl15.m3();
+        interfaceImpl15.m4();
+        interfaceImpl15.m5();
+
+        Interface16 interface16 = new Interface_Impl16();
+        interface16.m1();
+        interface16.m2();
+        interface16.m3();
+        interface16.m4();
+        interface16.m5();
+        interface16.m6();
+        interface16.m7();
+
+        Interface_Impl16 interfaceImpl16 = new Interface_Impl16();
+        interfaceImpl16.m1();
+        interfaceImpl16.m2();
+        interfaceImpl16.m3();
+        interfaceImpl16.m4();
+        interfaceImpl16.m5();
+        interfaceImpl16.m6();
+        interfaceImpl16.m7();
+
+        Interface17 interface17 = new Interface_Impl17();
+        interface17.m1();
+        interface17.m2();
+        interface17.m3();
+        interface17.m4();
+        interface17.m5();
+        interface17.m6();
+        interface17.m7();
+        Interface_Impl17 interfaceImpl17 = new Interface_Impl17();
+        interfaceImpl17.m1();
+        interfaceImpl17.m2();
+        interfaceImpl17.m3();
+        interfaceImpl17.m4();
+        interfaceImpl17.m5();
+        interfaceImpl17.m6();
+        interfaceImpl17.m7();
+
+        Interface18 interface18 = new Interface_Impl18();
+        interface18.m1();
+        interface18.m2();
+        interface18.m3();
+        interface18.m4();
+        interface18.m5();
+        interface18.m6();
+        interface18.m7();
+        Interface_Impl18 interfaceImpl18 = new Interface_Impl18();
+        interfaceImpl18.m1();
+        interfaceImpl18.m2();
+        interfaceImpl18.m3();
+        interfaceImpl18.m4();
+        interfaceImpl18.m5();
+        interfaceImpl18.m6();
+        interfaceImpl18.m7();
+
+        Interface19 interface19 = new Interface_Impl19();
+        interface19.m1();
+        interface19.m2();
+        interface19.m3();
+        interface19.m4();
+        interface19.m5();
+        interface19.m6();
+        interface19.m7();
+        Interface_Impl19 interfaceImpl19 = new Interface_Impl19();
+        interfaceImpl19.m1();
+        interfaceImpl19.m2();
+        interfaceImpl19.m3();
+        interfaceImpl19.m4();
+        interfaceImpl19.m5();
+        interfaceImpl19.m6();
+        interfaceImpl19.m7();
+
+        Interface20 interface20 = new Interface_Impl20();
+        interface20.m1();
+        interface20.m2();
+        interface20.m3();
+        interface20.m4();
+        interface20.m5();
+        interface20.m6();
+        interface20.m7();
+        Interface_Impl20 interfaceImpl20 = new Interface_Impl20();
+        interfaceImpl20.m1();
+        interfaceImpl20.m2();
+        interfaceImpl20.m3();
+        interfaceImpl20.m4();
+        interfaceImpl20.m5();
+        interfaceImpl20.m6();
+        interfaceImpl20.m7();
+
+        Interface21 interface21 = new Interface_Impl21();
+        interface21.m1();
+        interface21.m2();
+        interface21.m3();
+        interface21.m4();
+        interface21.m5();
+        interface21.m6();
+        interface21.m7();
+        interface21.m8();
+        interface21.m9();
+
+        Interface_Impl21 interfaceImpl21 = new Interface_Impl21();
+        interfaceImpl21.m1();
+        interfaceImpl21.m2();
+        interfaceImpl21.m3();
+        interfaceImpl21.m4();
+        interfaceImpl21.m5();
+        interfaceImpl21.m6();
+        interfaceImpl21.m7();
+        interfaceImpl21.m8();
+        interfaceImpl21.m9();
+
+        Interface22 interface22 = new Interface_Impl22();
+        interface22.m1();
+        interface22.m2();
+        interface22.m3();
+        interface22.m4();
+        interface22.m5();
+        interface22.m6();
+        interface22.m7();
+        interface22.m8();
+        interface22.m9();
+
+        Interface_Impl22 interfaceImpl22 = new Interface_Impl22();
+        interfaceImpl22.m1();
+        interfaceImpl22.m2();
+        interfaceImpl22.m3();
+        interfaceImpl22.m4();
+        interfaceImpl22.m5();
+        interfaceImpl22.m6();
+        interfaceImpl22.m7();
+        interfaceImpl22.m8();
+        interfaceImpl22.m9();
+
+        Interface23 interface23 = new Interface_Impl23();
+        interface23.m1();
+        interface23.m2();
+        interface23.m3();
+        interface23.m4();
+        interface23.m5();
+        interface23.m6();
+        interface23.m7();
+        interface23.m8();
+        interface23.m9();
+
+        Interface_Impl23 interfaceImpl23 = new Interface_Impl23();
+        interfaceImpl23.m1();
+        interfaceImpl23.m2();
+        interfaceImpl23.m3();
+        interfaceImpl23.m4();
+        interfaceImpl23.m5();
+        interfaceImpl23.m6();
+        interfaceImpl23.m7();
+        interfaceImpl23.m8();
+        interfaceImpl23.m9();
+
+        Interface24 interface24 = new Interface_Impl24();
+        interface24.m1();
+        interface24.m2();
+        interface24.m3();
+        interface24.m4();
+        interface24.m5();
+        interface24.m6();
+        interface24.m7();
+        interface24.m8();
+        interface24.m9();
+
+        Interface_Impl24 interfaceImpl24 = new Interface_Impl24();
+        interfaceImpl24.m1();
+        interfaceImpl24.m2();
+        interfaceImpl24.m3();
+        interfaceImpl24.m4();
+        interfaceImpl24.m5();
+        interfaceImpl24.m6();
+        interfaceImpl24.m7();
+        interfaceImpl24.m8();
+        interfaceImpl24.m9();
+
+
+        Interface25 interface25 = new Interface_Impl25();
+        interface25.m1();
+        interface25.m2();
+        interface25.m3();
+        interface25.m4();
+        interface25.m5();
+        interface25.m6();
+        interface25.m7();
+        interface25.m8();
+        interface25.m9();
+
+        Interface_Impl25 interfaceImpl25 = new Interface_Impl25();
+        interfaceImpl25.m1();
+        interfaceImpl25.m2();
+        interfaceImpl25.m3();
+        interfaceImpl25.m4();
+        interfaceImpl25.m5();
+        interfaceImpl25.m6();
+        interfaceImpl25.m7();
+        interfaceImpl25.m8();
+        interfaceImpl25.m9();
+
+
+    }
+}
